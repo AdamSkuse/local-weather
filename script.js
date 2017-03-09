9df7e08718aa111976325c2d3af280fef3bc1129
AdamSkuse <a.t.skuse@gmail.com>
2017-03-09 17:18:16 +0000

    Adds temperature and weather description

--- script.js
+++ script.js
@@ -8,12 +8,22 @@ function getCoords(latitude, longitude) {
     console.log (lat, lon);
     $.getJSON("http://api.openweathermap.org/data/2.5/weather?APPID=5477c7f4fa528d2f1048c4cb827c4267&lat=" + lat + "&lon=" + lon, function(json) {
     console.log(JSON.stringify(json));
-    displayWeatherIcon(json.weather[0].id);
+    displayWeatherIcon(json.weather[0].id, json.weather[0].description, json.main.temp);
     });
 }
 
-function displayWeatherIcon(id) {
-    console.log(id);
+function displayWeatherIcon(id, description, temperature) {
+    console.log(id, description, temperature);
+    var weatherDescriptionDiv = document.getElementById("weatherDescription");
+    var weatherDescriptionP = document.createElement("p");
+    weatherDescriptionP.innerHTML = description;
+    weatherDescriptionDiv.appendChild(weatherDescriptionP);
+
+    var temperatureDiv= document.getElementById("temperature");
+    var temperatureP= document.createElement("p");
+    temperatureP.innerHTML = (temperature -273.15).toFixed(1);
+    temperatureDiv.appendChild(temperatureP);
+
     var iconCode = "";
     if (id > 199 && id < 233) {
         iconCode = "wi-thunderstorm";
